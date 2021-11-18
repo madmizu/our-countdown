052feb905acfd3cc7d88ccf8b31bf044a32b2a55
Madison Shimizu <mhshimizu@outlook.com>
2021-11-17 20:09:50 -1000

    added cs style sheet and js script

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener ("DOMContentLoaded", ()=>{})
+
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+document.querySelector("body").appendChild(h2);
