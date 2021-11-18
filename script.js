216406753764f77c49dfa4e87c65fadae27b5c1e
Madison Shimizu <mhshimizu@outlook.com>
2021-11-17 20:24:53 -1000

    countdown timer

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-document.addEventListener ("DOMContentLoaded", ()=>{})
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const newYears = "24 Dec 2021 06:20:00";
 
-document.querySelector("body").appendChild(h2);
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    console.log (time < 10 ? `0${time}` : time)
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
